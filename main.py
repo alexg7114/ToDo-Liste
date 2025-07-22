8fd9ffdc6b6f3fc25c125101a577907ec7e7f669
Alex Gaus <alexg7114@gmail.com>
2025-07-22 23:52:03 +0200

    Add login/register with JWT, fix daytime error, include venv and debugging prints

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from passlib.context import CryptContext
 from fastapi import HTTPException
+from jose import JWTError, jwt
+from datetime import datetime, timedelta, timezone
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -40,3 +53,76 @@ def register(user: UserIn):
   return {"username": user.username}
 
 
+SECRET_KEY = "secret-key-should-be-long-and-random"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+  print("Generating token for:", data)
+  to_encode = data.copy()
+  expire = datetime.now(tz=timezone.utc) + (expires_delta or timedelta(minutes=15))
+  print("Expire at:", expire)
+  to_encode.update({"exp": expire})
+  token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+  print("Generated token:", token)
+  return token
+
+
+@app.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+  print("Recieved login form data:")
+  print("Username:", form_data.username)
+  print("Password:", form_data.password)
+  
+  user = None
+  for u in users_db:
+    print("Checking user:", u["username"])
+    if u["username"] == form_data.username:
+      user = u
+      break
+    
+  
+  if user is None:
+    print("User not found!")
+    raise HTTPException(status_code=401, detail="Incorrect username or password")
+  
+  print("User found:", user)
+  print("Comparing password...")
+  
+  if not pwd_context.verify(form_data.password, user["hashed_password"]):
+    print("Password incorrect!")
+    raise HTTPException(status_code=401, detail="Incorrect username or password")
+  
+  print("Password correct. Creating token...")
+  
+  token = create_access_token(data={"sub": user["username"]})
+  print("Token created:", token)
+  return {"access_token": token, "token_type": "bearer"}
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+  credentials_exception = HTTPException(status_code=401, detail="Invalid credentials")
+  try:
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    username = payload.get("sub")
+    if username is None:
+      raise credentials_exception
+  except JWTError:
+    raise credentials_exception
+  
+  user = None
+  for u in users_db:
+    if u["username"] == username:
+      user = u
+      break
+    
+  if user is None:
+    raise credentials_exception
+  return user
+
+print("Current users_db:", users_db)
+
+
+  
+  
