59b9d9a4ca11d3d26f8c5627c7b779266b03914f
Alex Gaus <alexg7114@gmail.com>
2025-07-24 10:23:17 +0200

    Refactored authentication to use OAuth2PasswordRequestForm for Swagger UI login

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from jose import JWTError, jwt
 from datetime import datetime, timedelta, timezone
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import Depends
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+#from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import logging
 
 from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
@@ -26,13 +26,14 @@ from database import get_session
 from models import User, UserCreate, UserRead
 from models import ToDoUpdate
 from fastapi import status
+from fastapi.security import OAuth2PasswordBearer
 
-security = HTTPBearer()
+#security = HTTPBearer()
 
 
 logging.basicConfig(level=logging.DEBUG)
 
-oauth2_scheme = HTTPBearer()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 app = FastAPI()
 
@@ -111,9 +112,8 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = D
 
 
 
-def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security), session: Session = Depends(get_session)):
-  token = credentials.credentials
-  
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    
   try:
     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     username = payload.get("sub")
@@ -122,7 +122,9 @@ def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(securit
   except JWTError:
     raise HTTPException(status_code=401, detail="Token decoding failed")
   
-  user = session.exec(select(User).where(User.username == username)).first()
+  with Session(engine) as session:
+    statement = select(User).where(User.username == username)
+    user = session.exec(statement).first()
   if user is None:
     raise HTTPException(status_code=401, detail="User not found")
   return user
